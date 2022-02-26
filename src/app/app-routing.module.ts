8e778a1f618060f7c25f5bb595289db071075666
Moreno <morcar@americas.corpdir.net>
2022-02-26 13:16:08 -0600

    default routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,8 @@ import { LeagueComponent } from './pages/league/league.component';
 const routes: Routes = [
   {path: 'home', loadChildren: () => import('src/app/pages/pages.module').then((m) => m.PagesModule), component: HomeComponent},
   {path: 'league', loadChildren: () => import('src/app/pages/pages.module').then((m) => m.PagesModule), component: LeagueComponent},
+  {path: '**', loadChildren: () => import('src/app/pages/pages.module').then((m) => m.PagesModule), component: HomeComponent},
+
 ];
 
 @NgModule({
